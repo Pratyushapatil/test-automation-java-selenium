1540cbb74d09ddac5c3127e9cc4845e9fc67017c
pratyusha-codes <98pratyushapatil@gmail.com>
2023-10-13 17:26:46 +0800

    Init-automation project setup using java and selenium

---
+++ src/Browsepackage/BrowseClass.java
@@ -0,0 +1,14 @@
+package Browsepackage;
+
+
+import org.openqa.selenium.WebDriver;
+
+import Driverpackage.DriverClass;
+
+
+
+public class BrowseClass extends DriverClass{
+	WebDriver driver ;
+    
+	
+}
